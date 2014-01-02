5a52e28107665646315d0fdc2060b19efda14fe5
Nada Amin <livecode@namin.net>
2014-01-02 17:21:57 +0000

    started handling sites

--- pub/__init__.py
+++ pub/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import render_template_string
 from flask import jsonify
 from flask import json
+from jinja2.exceptions import TemplateSyntaxError
 
 app = Flask(__name__)
 app.config.from_envvar('APP_SETTINGS')
@@ -61,22 +63,56 @@ def github_git_url(user, repo):
 def github_defaults_url(user, repo):
     return 'https://raw.github.com/%s/%s/master/.io.livecode.ch/defaults.json' % (user, repo)
 
-@app.route("/repl/<user>/<repo>")
-def www_github_repl(user, repo):
+def github_site_index_url(user, repo):
+    return 'https://raw.github.com/%s/%s/master/.io.livecode.ch/_site/index.html' % (user, repo)
+
+def github_site_index_src_link(user, repo):
+    return 'https://github.com/%s/%s/tree/master/.io.livecode.ch/_site/index.html' % (user, repo)
+
+class UserError(Exception):
+    def __init__(self, user, repo, template_file='error_livecode_config.html', status_code=500, ctx=None, err=None):
+        self.user = user
+        self.repo = repo
+        self.template_file = template_file
+        self.status_code = status_code
+        self.ctx = ctx
+        self.err = err
+
+@app.errorhandler(UserError)
+def handle_user_error(e):
+    return render_template(e.template_file, user=e.user, repo=e.repo, status=e.status_code, ctx=e.ctx, err=e.err), e.status_code
+
+def fetch_defaults(user, repo):
     r_check = requests.head(github_check_url(user, repo))
-    if r_check.status_code == 404:
-        return render_template('error_repo_not_found.html', status=r_check.status_code, user=user, repo=repo), r_check.status_code
+    if r_check.status_code != 200:
+        raise UserError(user, repo, 'error_repo_not_found.html', r_check.status_code)
     r_defaults = requests.get(github_defaults_url(user, repo))
-    if r_defaults.status_code == 404:
-        return render_template('error_livecode_not_found.html', status=r_defaults.status_code, user=user, repo=repo), r_defaults.status_code
+    if r_defaults.status_code != 200:
+        raise UserError(user, repo, 'error_livecode_not_found.html', r_defaults.status_code)
     try:
         j_defaults = r_defaults.json()
     except ValueError as e:
-        return render_template('error_livecode_config.html', user=user, repo=repo, status=500, ctx='while parsing <code>defaults.json</code>', err=str(e)), 500
+        raise UserError(user, repo, ctx='while parsing <code>defaults.json</code>', err=str(e))
     o = dkr_check_img(github_dkr_img(user, repo), github_git_url(user, repo), refresh=request.args.get('refresh', False))
     if o['status']!=0:
-        return render_template('error_livecode_config.html', user=user, repo=repo, status=500, ctx='while installing', err=o.out), 500
-    return render_template('repl.html', user=user, repo=repo, language= j_defaults.get('language'))
+        raise UserError(user, repo, ctx='while installing', err=o.out)
+    return j_defaults
+
+@app.route("/repl/<user>/<repo>")
+def www_github_repl(user, repo):
+    j_defaults = fetch_defaults(user, repo)
+    return render_template('repl.html', user=user, repo=repo, language=j_defaults.get('language'))
+
+@app.route("/learn/<user>/<repo>")
+def www_github_learn(user, repo):
+    j_defaults = fetch_defaults(user, repo)
+    r_index = requests.get(github_site_index_url(user, repo))
+    if r_index.status_code != 200:
+        raise UserError(user, repo, 'error_site_not_found.html', r_index.status_code)
+    try:
+        return render_template_string(r_index.text, user=user, repo=repo, language=j_defaults.get('language'))
+    except TemplateSyntaxError as e:
+        raise UserError(user, repo, ctx='while rendering <a href="%s">the site index</a>' % github_site_index_src_link(user, repo), err=str(e))
 
 @app.route("/api/run/<user>/<repo>", methods=['POST'])
 def github_run(user, repo):
@@ -115,7 +151,7 @@ def index():
     return render_template('index.html')
 
 @app.errorhandler(404)
-def page_not_found(e):
+def handle_page_not_found(e):
     return render_template('error_404.html', status=e), 404
 
 if __name__ == "__main__":
